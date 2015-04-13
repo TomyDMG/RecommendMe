23b6bf39c83230e95d728bfedeed7224ab1496df
TomyDMG <tomydmg@gmail.com>
2015-04-13 13:30:08 +0000

    sing-up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,23 @@
 class User < ActiveRecord::Base
-
 before_save { self.email = email.downcase }
+before_create :create_remember_token
 validates :name, length: { maximum: 50}
 validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: { case_sensitive: false }
 has_secure_password
 validates :password, length: { minimum: 6 }
 validates_presence_of :name, :email   
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.encrypt(User.new_remember_token)
+    end
 end
